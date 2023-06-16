b9d7ab92635b35bcdc71b1f3bfae946f2c09358e
MD Farhad Jaman <jamanfarhad1@gmail.com>
2023-06-16 21:56:42 +0600

    Removed build file

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.d41669ef.js","_app/immutable/chunks/index.ad50d19f.js","_app/immutable/chunks/stores.447d0093.js","_app/immutable/chunks/singletons.b925919d.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/5.js
+++
@@ -1,9 +0,0 @@
-import * as server from '../entries/pages/(auth)/register/_page.server.ts.js';
-
-export const index = 5;
-export const component = async () => (await import('../entries/pages/(auth)/register/_page.svelte.js')).default;
-export { server };
-export const server_id = "src/routes/(auth)/register/+page.server.ts";
-export const imports = ["_app/immutable/nodes/5.3012da3a.js","_app/immutable/chunks/index.ad50d19f.js","_app/immutable/chunks/forms.d7b031ff.js","_app/immutable/chunks/parse.bee59afc.js","_app/immutable/chunks/singletons.b925919d.js"];
-export const stylesheets = [];
-export const fonts = [];
